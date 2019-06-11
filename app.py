3404fb1f998bc2bb9d603f990d93fa3115c63f2e
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2019-06-10 22:35:49 -0300

    it's almost working

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+
 import os
 import codecs
 from uuid import uuid4,UUID
--- static/js/encrypt.js
+++ static/js/encrypt.js
@@ -1,15 +1,14 @@
 function encrypt() {
   var rawfile = document.getElementById("file")["files"][0];
   var reader = new FileReader();
-  var enc_content = "";
-  //var password = document.getElementById("password").value
-  var password = "admin"
   var lastlength = 0;
   var count = 1;
+  //var password = document.getElementById("password").value
+  var password_string = "admin"
 
   clearInterval(passwordgenerator);
   $('html').loading({theme: 'dark'})
-  var fname_enc = CryptoJS.AES.encrypt(rawfile.name, password).toString();
+  var fname_enc = CryptoJS.AES.encrypt(rawfile.name, password_string).toString();
   var limit = document.getElementById("limit-downloads").value
   $.ajax({
     method: "POST",
@@ -19,12 +18,12 @@ function encrypt() {
     success: function (response){
       var fid = response["fid"]
       onload(fid)
-      reader.readAsArrayBuffer(rawfile)
+      //reader.readAsArrayBuffer(rawfile,"utf-8")
+      reader.readAsDataURL(rawfile,"utf-8")
     }
   });
 
   function sendToServer(binary,fid,count){
-    console.log(binary)
     var xhr = new XMLHttpRequest();
     xhr.open("PUT", "/api/upload/"+fid+"/"+count,false);
     xhr.setRequestHeader("Content-Type", "application/octet-stream");
@@ -33,30 +32,36 @@ function encrypt() {
 
   function onload (fid){
     reader.onload = function (e){
-      console.log(e)
-      console.log(Math.round((e.loaded /1000000))+ "Mb/" + Math.round((e.total/1000000))+"Mb")
+      var enc_content = ""
       var binary = ""
-      var bytes = new Uint8Array(e.target.result)
-      var length = bytes.byteLength;
-      var CHUNK_SIZE = 4096 * 1000
+      var bytes = reader.result
+      var length = bytes.length
+      var CHUNK_SIZE = 2048 * 1000
       var count = 1
-      for (var i = 0; i < length; i++) {
-        binary += String.fromCharCode(bytes[i]);
-        //binary += bytes[i]
-        //if(binary.length >= CHUNK_SIZE){
-        //  enc_content += CryptoJS.AES.encrypt(binary, password).toString(CryptoJS.enc.Utf8);
-        //  console.log(enc_content)
-        //  sendToServer(enc_content,fid,count)
-        //  count += 1
-        //  binary = ""
-        //}
+      if(length <= CHUNK_SIZE){
+        console.log("menor")
+        //wordarray = CryptoJS.lib.WordArray.create(bytes)
+        //enc_content = CryptoJS.AES.encrypt(wordarray, password_string).toString();
+        enc_content = CryptoJS.AES.encrypt(bytes, password_string).toString();
+        sendToServer(enc_content,fid,count)
+        console.log(bytes[0])
+      } else {
+        for (var i = 0; i < length; i++) {
+          binary += bytes[i]
+          if(binary.length >= CHUNK_SIZE){
+            enc_content = CryptoJS.AES.encrypt(binary, password_string).toString();
+            sendToServer(enc_content,fid,count)
+            enc_content = ""
+            binary = ""
+            count += 1
+          }
+        }
+        if(binary.length > 0){
+          console.log(binary.length)
+          enc_content = CryptoJS.AES.encrypt(binary, password_string).toString();
+          sendToServer(binary,fid,count)
+        }
       }
-      if(binary){
-        //enc_content += CryptoJS.AES.encrypt(binary, password).toString(CryptoJS.enc.Utf8);
-        //sendToServer(binary,fid,count)
-        download("file.jpg",binary)
-      }
-      console.log(binary)
       var xhr = new XMLHttpRequest();
       xhr.open("PATCH", "/api/upload/"+fid);
       xhr.send();
@@ -72,19 +77,14 @@ function encrypt() {
       }
     }
   }
-
-
-  //  function onload(fid){
-  //    reader.onload = function (e) {
-  //      console.log(e)
 }
 
 function decrypt(file){
   $('html').loading({theme: 'dark'})
-  var password = "admin"
+  var password_string = "admin"
   //var password = document.getElementById("password").value
   var reader = new FileReader();
-  var dec_content=""
+  var dec_content= ""
   var lastlength = 0;
   var fid = document.getElementById("fid").value
   var xhr = new XMLHttpRequest();
@@ -93,45 +93,46 @@ function decrypt(file){
   xhr.onreadystatechange = function (data) {
     if (xhr.readyState === 4) {
       var count = JSON.parse(data.target.response)["chunks"]
-      var filename = CryptoJS.AES.decrypt(JSON.parse(data.target.response)["fname"],password).toString(CryptoJS.enc.Utf8)
+      var filename = CryptoJS.AES.decrypt(JSON.parse(data.target.response)["fname"],password_string).toString(CryptoJS.enc.Utf8)
       for (var i=1; i <= count; i++){
         var xhrinside = new XMLHttpRequest();
+        console.log(i)
         xhrinside.open("GET", "/api/download/"+fid+"/"+i,false);
         xhrinside.onreadystatechange = function (data) {
           if (xhr.readyState === 4) {
-            //result = e.target.result.slice(lastlength,)
-            //lastlength = e.target.response.length
-            dec_content += CryptoJS.AES.decrypt(data.target.response, password).toString(CryptoJS.enc.Utf8)
-            //console.log(data.target.response)
-            //dec_content += data.target.response
+            console.log(fid)
+            if(data.target.response){
+              dec_content += CryptoJS.AES.decrypt(data.target.response, password_string).toString(CryptoJS.enc.Utf8)
+            }
           }
         }
         xhrinside.send();
       }
-      download(filename,dec_content)
-      console.log(dec_content)
+      //download(filename,dec_content)
       $('html').loading('stop')
     }
   }
 }
 
-function download(fname,fcontent){
-  console.log("Dowloading...")
-  var data = new Blob([fcontent],{type: 'application/octet-binary'})
-  var url = window.URL.createObjectURL(data);
-  var element = document.createElement('a');
-  element.setAttribute('href', url);
-  element.setAttribute('download', fname);
-  element.style.display = 'none';
-  console.log(element)
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
-}
-
-function hex2a(hex){
-  var str = '';
-  for (var i = 0; i < hex.length; i += 2)
-    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-  return str;
+//function download(fname,fcontent){
+//  console.log("Dowloading...")
+//  var data = new Blob([fcontent],{encoding: 'utf-8'})
+//  var url = window.URL.createObjectURL(data);
+//  var element = document.createElement('a');
+//  element.setAttribute('href', url);
+//  element.setAttribute('download', fname);
+//  element.style.display = 'none';
+//  console.log(element)
+//  document.body.appendChild(element);
+//  element.click();
+//  document.body.removeChild(element);
+//}
+function downloadURI(name,uri) {
+  var link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  delete link;
 }
